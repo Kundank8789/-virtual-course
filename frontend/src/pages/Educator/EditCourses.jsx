4ff374f49ae89dd3cbfb06a0bfcbf431a16f1894
kundan <kundan8789@gmail.com>
2025-09-18 23:42:53 +0530

    some change

--- frontend/src/pages/Educator/EditCourses.jsx
+++ frontend/src/pages/Educator/EditCourses.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function EditCourses() {
   return (
     <div className='max-w-5xl mx-auto p-6 mt-10 bg-white rounded-lg shadow-md'>
+      {/* top bar */}
+
+      <div className='flex items-center justify-between gap-[20px] md:justify-between flex-col md:flex-row mb-6 relative'>
+
       </div>
+      {/* form detail */}
+
+
+      </div> 
   )
 }
 
