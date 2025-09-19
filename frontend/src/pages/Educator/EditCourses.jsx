ec62e49fd9495a4b2ec1666c94e4eaf8e9f317c9
kundan <kundan8789@gmail.com>
2025-09-19 23:22:52 +0530

    add some feature

--- frontend/src/pages/Educator/EditCourses.jsx
+++ frontend/src/pages/Educator/EditCourses.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import {FaArrowLeftLong} from 'react-icons/fa6'
 function EditCourses() {
   return (
     <div className='max-w-5xl mx-auto p-6 mt-10 bg-white rounded-lg shadow-md'>
       {/* top bar */}
 
       <div className='flex items-center justify-between gap-[20px] md:justify-between flex-col md:flex-row mb-6 relative'>
+        <FaArrowLeftLong className='top-[20%] md:top-[20%] absolute left-[0] md:left-[2%] w-[22px] h-[22px] cursor-pointer '/>
 
       </div>
       {/* form detail */}
+
 
 
       </div> 
